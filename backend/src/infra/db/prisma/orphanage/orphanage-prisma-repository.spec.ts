f450c3bb6b9ea151045aa11389df7a993e78b529
Lucas William <lucaswilliameufrasio@gmail.com>
2020-10-17 01:45:38 -0300

    test: ensure OrphanagePrismaRepository returns an orphanage on success

---
+++ backend/src/infra/db/prisma/orphanage/orphanage-prisma-repository.spec.ts
@@ -0,0 +1,36 @@
+import { OrphanagePrismaRepository } from './orphanage-prisma-repository'
+import { prisma } from '@/infra/db/prisma/helpers/prisma-helper'
+import { mockAddOrphanageParams } from '@/domain/test'
+import { AddOrphanageParams } from '@/domain/usecases/orphanage/add-orphanage'
+import faker from 'faker'
+
+const mockAddOrphanagePrisma = (orphanage: AddOrphanageParams): void => {
+  prisma.orphanage.create = jest.fn().mockImplementationOnce(() => Object.assign({}, orphanage, { id: faker.random.number() }))
+}
+
+const makeSut = (): OrphanagePrismaRepository => {
+  return new OrphanagePrismaRepository()
+}
+
+describe('OrphanagePrismaRepository', () => {
+  describe('add()', () => {
+    test('Should return an orphanage on success', async () => {
+      const sut = makeSut()
+
+      const addOrphanageParams = mockAddOrphanageParams()
+
+      mockAddOrphanagePrisma(addOrphanageParams)
+
+      const orphanage = await sut.add(addOrphanageParams)
+
+      expect(orphanage).toBeTruthy()
+      expect(orphanage.name).toBe(addOrphanageParams.name)
+      expect(orphanage.longitude).toBe(addOrphanageParams.longitude)
+      expect(orphanage.latitude).toBe(addOrphanageParams.latitude)
+      expect(orphanage.instructions).toBe(addOrphanageParams.instructions)
+      expect(orphanage.whatsapp).toBe(addOrphanageParams.whatsapp)
+      expect(orphanage.about).toBe(addOrphanageParams.about)
+      expect(orphanage.open_on_weekend).toBe(addOrphanageParams.open_on_weekend)
+    })
+  })
+})
