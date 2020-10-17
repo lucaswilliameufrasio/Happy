28f5bc283e47f9c938aec67252fbfa4053b8a175
Lucas William <lucaswilliameufrasio@gmail.com>
2020-10-17 05:37:16 -0300

    refactor: move files to their own folder

--- backend/src/data/usecases/orphanage/add-orphanage/db-add-orphanage-protocols.ts
+++ backend/src/data/usecases/orphanage/add-orphanage/db-add-orphanage-protocols.ts
@@ -1,3 +1,3 @@
-export * from '@/data/protocols/db/add-orphanage-repository'
+export * from '@/data/protocols/db/orphanage/add-orphanage-repository'
 export * from '@/domain/models/orphanage'
 export * from '@/domain/usecases/orphanage/add-orphanage'
