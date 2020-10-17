4989fbad00c0e5a711ea83486a0ae35608c58bfd
Lucas William <lucaswilliameufrasio@gmail.com>
2020-10-17 02:08:37 -0300

    feat: ensure AddOrphanageController returns 400 if Validation fails

--- backend/src/presentation/controllers/orphanage/add-orphanage/add-orphanage-controller.ts
+++ backend/src/presentation/controllers/orphanage/add-orphanage/add-orphanage-controller.ts
@@ -1,10 +1,16 @@
+import { badRequest } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, Validation } from './add-orphanage-controller-protocols'
 
 export class AddOrphanageController implements Controller {
   constructor (private readonly validation: Validation) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.body)
+    const error = this.validation.validate(httpRequest.body)
+
+    if (error) {
+      return badRequest(error)
+    }
+
     return null
   }
 }
