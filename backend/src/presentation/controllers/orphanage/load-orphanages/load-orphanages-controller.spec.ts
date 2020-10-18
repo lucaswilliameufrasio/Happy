72fcaf6da0f7a3ae38922dfbd5fa0df5b255c261
Lucas William <lucaswilliameufrasio@gmail.com>
2020-10-18 15:33:49 -0300

    test: ensure LoadOrphanagesController returns 200 on success

--- backend/src/presentation/controllers/orphanage/load-orphanages/load-orphanages-controller.spec.ts
+++ backend/src/presentation/controllers/orphanage/load-orphanages/load-orphanages-controller.spec.ts
@@ -1,5 +1,6 @@
 import { LoadOrphanagesController } from './load-orphanages-controller'
 import { LoadOrphanagesSpy } from '@/presentation/test'
+import { ok } from '@/presentation/helpers/http/http-helper'
 
 type SutTypes = {
   sut: LoadOrphanagesController
@@ -23,5 +24,12 @@ describe('LoadOrphanages Controller', () => {
     await sut.handle({})
 
     expect(loadOrphanagesSpy.calls).toBe(1)
+  })
+
+  test('Should return 200 on success', async () => {
+    const { sut, loadOrphanagesSpy } = makeSut()
+    const httpResponse = await sut.handle({})
+
+    expect(httpResponse).toEqual(ok(loadOrphanagesSpy.orphanageModel))
   })
 })
