10d8537e1b16a3a98e8a7b3c7a2cec693a56ba7b
Lucas William <lucaswilliameufrasio@gmail.com>
2020-10-18 02:16:55 -0300

    feat: ensure DbAddOrphanage returns an orphanage on success

--- backend/src/data/usecases/orphanage/add-orphanage/db-add-orphanage.ts
+++ backend/src/data/usecases/orphanage/add-orphanage/db-add-orphanage.ts
@@ -3,8 +3,8 @@ import { AddOrphanageRepository, OrphanageModel, AddOrphanage, AddOrphanageParam
 export class DbAddOrphanage implements AddOrphanage {
   constructor (private readonly addOrphanageRepository: AddOrphanageRepository) {}
 
-  async add (orphanage: AddOrphanageParams): Promise<OrphanageModel> {
-    await this.addOrphanageRepository.add(orphanage)
-    return null
+  async add (orphanageData: AddOrphanageParams): Promise<OrphanageModel> {
+    const orphanage = await this.addOrphanageRepository.add(orphanageData)
+    return orphanage
   }
 }
