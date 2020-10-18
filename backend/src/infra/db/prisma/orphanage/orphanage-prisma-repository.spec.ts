37a7e32f200cd2005bd31eeb1e7169c898170e6b
Lucas William <lucaswilliameufrasio@gmail.com>
2020-10-18 13:06:06 -0300

    test: ensure OrphanagePrismaRepository find orphanage by id with success

--- backend/src/infra/db/prisma/orphanage/orphanage-prisma-repository.spec.ts
+++ backend/src/infra/db/prisma/orphanage/orphanage-prisma-repository.spec.ts
@@ -1,13 +1,18 @@
 import { OrphanagePrismaRepository } from './orphanage-prisma-repository'
 import { prisma } from '@/infra/db/prisma/helpers/prisma-helper'
-import { mockAddOrphanageParams } from '@/domain/test'
+import { mockAddOrphanageParams, mockOrphanageModel } from '@/domain/test'
 import { AddOrphanageParams } from '@/domain/usecases/orphanage/add-orphanage'
+import { OrphanageModel } from '@/domain/models/orphanage'
 import faker from 'faker'
 
 const mockAddOrphanagePrisma = (orphanage: AddOrphanageParams): void => {
   const orphanageParams = Object.assign({}, orphanage, { id: faker.random.number() })
-  prisma.orphanage.create = jest.fn().mockReturnValueOnce(orphanageParams)
+  prisma.orphanage.create = jest.fn().mockReturnValueOnce(orphanage)
   prisma.orphanage.findOne = jest.fn().mockReturnValueOnce(orphanageParams)
+}
+
+const mockLoadOrphanageByIdPrisma = (orphanage: OrphanageModel): void => {
+  prisma.orphanage.findOne = jest.fn().mockReturnValueOnce(orphanage)
 }
 
 const makeSut = (): OrphanagePrismaRepository => {
@@ -33,6 +38,20 @@ describe('OrphanagePrismaRepository', () => {
       expect(orphanage.whatsapp).toBe(addOrphanageParams.whatsapp)
       expect(orphanage.about).toBe(addOrphanageParams.about)
       expect(orphanage.open_on_weekend).toBe(addOrphanageParams.open_on_weekend)
+    })
+  })
+
+  describe('loadById()', () => {
+    test('Should load orphanage by id on success', async () => {
+      const sut = makeSut()
+      const OrphanageModel = mockOrphanageModel()
+
+      mockLoadOrphanageByIdPrisma(OrphanageModel)
+
+      const orphanage = await sut.loadById(OrphanageModel.id)
+
+      expect(orphanage).toBeTruthy()
+      expect(orphanage.id).toBeTruthy()
     })
   })
 })
