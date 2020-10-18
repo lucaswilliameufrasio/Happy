7c4238356c714a4d4ee6523d53e626c2eac3a962
Lucas William <lucaswilliameufrasio@gmail.com>
2020-10-17 22:39:58 -0300

    test: ensure ValidationComposite return an error if any validation fails

---
+++ backend/src/validation/validators/validation-composite.spec.ts
@@ -0,0 +1,36 @@
+import { ValidationComposite } from './validation-composite'
+import { ValidationSpy } from '@/presentation/test'
+import { MissingParamError } from '@/presentation/errors'
+import faker from 'faker'
+
+const field = faker.random.word()
+
+type SutTypes = {
+  sut: ValidationComposite
+  validationSpies: ValidationSpy[]
+}
+
+const makeSut = (): SutTypes => {
+  const validationSpies = [
+    new ValidationSpy(),
+    new ValidationSpy()
+  ]
+  const sut = new ValidationComposite(validationSpies)
+
+  return {
+    sut,
+    validationSpies
+  }
+}
+
+describe('Validation Composite', () => {
+  test('Should return an error if any validation fails', () => {
+    const { sut, validationSpies } = makeSut()
+
+    validationSpies[1].error = new MissingParamError(field)
+
+    const error = sut.validate({ [field]: faker.random.word() })
+
+    expect(error).toEqual(validationSpies[1].error)
+  })
+})
