2881d615ced615167701c97d33eab47597b9c911
Lucas William <lucaswilliameufrasio@gmail.com>
2020-10-18 14:01:42 -0300

    test: ensure LoadOrphanageByIdController returns 500 if LoadOrphanageById throws

--- backend/src/presentation/controllers/orphanage/load-orphanage-by-id/load-orphanage-by-id-controller.spec.ts
+++ backend/src/presentation/controllers/orphanage/load-orphanage-by-id/load-orphanage-by-id-controller.spec.ts
@@ -1,6 +1,8 @@
 import { LoadOrphanageByIdController } from './load-orphanage-by-id-controller'
 import { HttpRequest } from '@/presentation/protocols/http'
 import { LoadOrphanageByIdSpy } from '@/presentation/test'
+import { serverError } from '@/presentation/helpers/http/http-helper'
+import { throwError } from '@/domain/test'
 import faker from 'faker'
 
 const mockRequest = (): HttpRequest => ({
@@ -33,5 +35,14 @@ describe('LoadOrphanageById Controller', () => {
     await sut.handle(httpRequest)
 
     expect(loadOrphanageByIdSpy.id).toEqual(httpRequest.params.orphanageId)
+  })
+
+  test('Should return 500 if LoadOrphanageById throws', async () => {
+    const { sut, loadOrphanageByIdSpy } = makeSut()
+    jest.spyOn(loadOrphanageByIdSpy, 'loadById').mockImplementationOnce(throwError)
+
+    const httpResponse = await sut.handle(mockRequest())
+
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
