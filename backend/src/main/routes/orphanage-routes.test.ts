8cd746e5b4544e8c3478bddbcdf4810810654f3b
Lucas William <lucaswilliameufrasio@gmail.com>
2020-10-17 23:26:53 -0300

    refactor: mock orphanage prisma before integration tests

--- backend/src/main/routes/orphanage-routes.test.ts
+++ backend/src/main/routes/orphanage-routes.test.ts
@@ -1,8 +1,19 @@
-import { getAssetPathHelper } from '@/shared/test/helpers/get-asset-path-helper'
 import request from 'supertest'
-import app from '../config/app'
+import faker from 'faker'
+import app from '@/main/config/app'
+import { prisma } from '@/infra/db/prisma/helpers/prisma-helper'
+import { getAssetPathHelper } from '@/shared/test/helpers/get-asset-path-helper'
+import { mockAddOrphanageParams } from '@/domain/test'
+
+const mockOrphanagePrisma = (): void => {
+  const orphanageParams = Object.assign({}, mockAddOrphanageParams(), { id: faker.random.number() })
+  prisma.orphanage.create = jest.fn().mockReturnValueOnce(orphanageParams)
+  prisma.orphanage.findOne = jest.fn().mockReturnValueOnce(orphanageParams)
+}
 
 describe('Orphanage Routes', () => {
+  beforeAll(mockOrphanagePrisma)
+
   test('Should return 201 on add survey success', async () => {
     await request(app)
       .post('/api/orphanages')
