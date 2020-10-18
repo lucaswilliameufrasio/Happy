c5d3a82dc1024c189d9520eb8ea5f80edbe8ce72
Lucas William <lucaswilliameufrasio@gmail.com>
2020-10-18 14:10:46 -0300

    feat: ensure LoadOrphanageByIdController returns 403 if LoadOrphanageById returns null

--- backend/src/presentation/controllers/orphanage/load-orphanage-by-id/load-orphanage-by-id-controller.ts
+++ backend/src/presentation/controllers/orphanage/load-orphanage-by-id/load-orphanage-by-id-controller.ts
@@ -1,5 +1,6 @@
 import { Controller, HttpRequest, HttpResponse } from './load-orphanage-by-id-controller-protocols'
-import { ok, serverError } from '@/presentation/helpers/http/http-helper'
+import { forbidden, ok, serverError } from '@/presentation/helpers/http/http-helper'
+import { InvalidParamError } from '@/presentation/errors'
 import { LoadOrphanageById } from '@/domain/usecases/orphanage/load-orphanage-by-id'
 
 export class LoadOrphanageByIdController implements Controller {
@@ -11,6 +12,9 @@ export class LoadOrphanageByIdController implements Controller {
     try {
       const { orphanageId } = httpRequest.params
       const orphanage = await this.loadOrphanageById.loadById(orphanageId)
+      if (!orphanage) {
+        return forbidden(new InvalidParamError('orphanageId'))
+      }
       return ok(orphanage)
     } catch (error) {
       return serverError(error)
