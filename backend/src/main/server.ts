f92742d2eb6676ee0741f13dfd5374dce695ca01
Lucas William <lucaswilliameufrasio@gmail.com>
2020-10-18 00:50:08 -0300

    feat: connect to db before app listen

--- backend/src/main/server.ts
+++ backend/src/main/server.ts
@@ -1,3 +1,12 @@
 import 'module-alias/register'
+import env from './config/env'
+import { prisma } from '@/infra/db/prisma/helpers/prisma-helper'
 
-app.listen(7777, () => console.log('Server running at http://localhost:7777'))
+prisma.$connect().then(async () => {
+  await prisma.$queryRaw('SELECT revision from "public"."_Migration"')
+  const app = (await import('./config/app')).default
+  app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+}).catch(async (error) => {
+  console.error(error)
+  await prisma.$disconnect()
+})
