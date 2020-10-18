5b9741e29339555adb8dbee38f8ebad20d9a442c
Lucas William <lucaswilliameufrasio@gmail.com>
2020-10-18 03:17:09 -0300

    refactor: change multer destination

--- backend/src/main/middlewares/multer.ts
+++ backend/src/main/middlewares/multer.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export const multerConfig = {
   storage: multer.diskStorage({
-    destination: path.join(__dirname, '..', '..', '..', 'uploads'),
+    destination: path.join(__dirname, '..', '..', 'uploads'),
     filename: (req, file, cb) => {
       req.socket.setTimeout(10 * 60 * 1000)
       cb(null, `${Date.now()}-${file.originalname}`)
