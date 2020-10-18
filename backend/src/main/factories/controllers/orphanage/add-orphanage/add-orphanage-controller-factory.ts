5a2d8befc2cbefff2f7772fb2131f2ab96d81882
Lucas William <lucaswilliameufrasio@gmail.com>
2020-10-18 14:12:58 -0300

    chore: move files to their own folder

--- backend/src/main/factories/controllers/orphanage/add-orphanage/add-orphanage-controller-factory.ts
+++ backend/src/main/factories/controllers/orphanage/add-orphanage/add-orphanage-controller-factory.ts
@@ -1,6 +1,6 @@
 import { AddOrphanageController } from '@/presentation/controllers/orphanage/add-orphanage/add-orphanage-controller'
 import { Controller } from '@/presentation/protocols'
-import { makeDbAddOrphanage } from '../../usecases/orphanage/add-orphanage/db-add-orphanage-factory'
+import { makeDbAddOrphanage } from '@/main/factories/usecases/orphanage/add-orphanage/db-add-orphanage-factory'
 import { makeAddOrphanageValidation } from './add-orphanage-validation-factory'
 
 export const makeAddOrphanageController = (): Controller => {
