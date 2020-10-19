a22c3a11962545ca462ef0d3a2d6c28615ebb523
Lucas William <lucaswilliameufrasio@gmail.com>
2020-10-19 11:22:21 -0300

    feat: ensure UpdateOrphanageController calls Validation with correct values

--- backend/src/presentation/controllers/orphanage/update-orphanage/update-orphanage-controller.ts
+++ backend/src/presentation/controllers/orphanage/update-orphanage/update-orphanage-controller.ts
@@ -1,12 +1,16 @@
-import { UpdateOrphanage, Controller, HttpRequest, HttpResponse, serverError, noContent } from './update-orphanage-controller-protocols'
+import { Validation, UpdateOrphanage, Controller, HttpRequest, HttpResponse, serverError, noContent } from './update-orphanage-controller-protocols'
 
 export class UpdateOrphanageController implements Controller {
   constructor (
+    private readonly validation: Validation,
     private readonly updateOrphanage: UpdateOrphanage
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const fields = { ...httpRequest.body, orphanageId: httpRequest.params }
+      this.validation.validate(fields)
+
       await this.updateOrphanage.update({ orphanageId: httpRequest.params.orphanageId, updateData: httpRequest.body })
       return noContent()
     } catch (error) {
