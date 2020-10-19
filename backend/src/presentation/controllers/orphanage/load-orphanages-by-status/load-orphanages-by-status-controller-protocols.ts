bfa3f7f17106a3702485872b4964bc7da2b2c9e1
Lucas William <lucaswilliameufrasio@gmail.com>
2020-10-19 05:23:08 -0300

    feat: ensure LoadOrphanagesByStatusController returns 403 if orphanageStatus is not boolean

--- backend/src/presentation/controllers/orphanage/load-orphanages-by-status/load-orphanages-by-status-controller-protocols.ts
+++ backend/src/presentation/controllers/orphanage/load-orphanages-by-status/load-orphanages-by-status-controller-protocols.ts
@@ -1,3 +1,4 @@
 export * from '@/domain/usecases/orphanage/load-orphanages-by-status'
 export * from '@/presentation/protocols'
 export * from '@/presentation/helpers/http/http-helper'
+export * from '@/presentation/errors'
--- backend/src/presentation/controllers/orphanage/load-orphanages-by-status/load-orphanages-by-status-controller.ts
+++ backend/src/presentation/controllers/orphanage/load-orphanages-by-status/load-orphanages-by-status-controller.ts
@@ -1,3 +1,5 @@
+import { forbidden } from '../add-orphanage/add-orphanage-controller-protocols'
+import { InvalidParamError } from '../load-orphanage-by-id/load-orphanage-by-id-controller-protocols'
 import { LoadOrphanagesByStatus, Controller, HttpRequest, HttpResponse, ok, noContent, serverError } from './load-orphanages-by-status-controller-protocols'
 
 export class LoadOrphanagesByStatusController implements Controller {
@@ -8,6 +10,10 @@ export class LoadOrphanagesByStatusController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { approvedStatus } = httpRequest.query
+
+      if (approvedStatus !== 'true' && approvedStatus !== 'false') {
+        return forbidden(new InvalidParamError('orphanageStatus'))
+      }
       const orphanages = await this.loadOrphanagesByStatus.loadByStatus(Boolean(approvedStatus))
 
       return orphanages.length ? ok(orphanages) : noContent()
