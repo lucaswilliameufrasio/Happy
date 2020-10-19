3479ea254d7cfb20dd85076e22d17306ff1822d8
Lucas William <lucaswilliameufrasio@gmail.com>
2020-10-19 04:13:51 -0300

    feat: ensure OrphanagePrismaRepository load orphanages by status on success

--- backend/src/infra/db/prisma/orphanage/orphanage-prisma-repository.ts
+++ backend/src/infra/db/prisma/orphanage/orphanage-prisma-repository.ts
@@ -4,9 +4,11 @@ import { OrphanageModel } from '@/domain/models/orphanage'
 import { AddOrphanageParams } from '@/domain/usecases/orphanage/add-orphanage'
 import { LoadOrphanageByIdRepository } from '@/data/protocols/db/orphanage/load-orphanage-by-id-repository'
 import { LoadOrphanagesRepository } from '@/data/protocols/db/orphanage/load-orphanages-repository'
+import { LoadOrphanagesByStatusRepository } from '@/data/protocols/db/orphanage/load-orphanages-by-status-repository'
 
-export class OrphanagePrismaRepository implements AddOrphanageRepository, LoadOrphanageByIdRepository, LoadOrphanagesRepository {
+export class OrphanagePrismaRepository implements AddOrphanageRepository, LoadOrphanageByIdRepository, LoadOrphanagesRepository, LoadOrphanagesByStatusRepository {
   constructor (private readonly appUrl: string) {}
+
   async add (data: AddOrphanageParams): Promise<OrphanageModel> {
     const { name, latitude, longitude, about, instructions, approved, opening_hours: openingHours, open_on_weekend: openOnWeekend, whatsapp, images } = data
     const createdOrphanage = await prisma.orphanage.create({
@@ -50,6 +52,20 @@ export class OrphanagePrismaRepository implements AddOrphanageRepository, LoadOr
 
   async load (): Promise<OrphanageModel[]> {
     const orphanages = await prisma.orphanage.findMany({
+      include: {
+        OrphanageImage: true
+      }
+    })
+
+    const orphanagesData = orphanages.map(orphanage => addImagesPropertyToOrphanageData(orphanage, this.appUrl))
+    return orphanagesData
+  }
+
+  async loadByStatus (approvedStatus: boolean): Promise<OrphanageModel[]> {
+    const orphanages = await prisma.orphanage.findMany({
+      where: {
+        approved: approvedStatus
+      },
       include: {
         OrphanageImage: true
       }
