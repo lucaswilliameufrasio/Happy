7150c1b36c6f13893f09a91d6c926ba70837f4aa
Lucas William <lucaswilliameufrasio@gmail.com>
2020-10-24 02:26:12 -0300

    chore: add config on jest to ignore node_modules

--- web/jest.config.js
+++ web/jest.config.js
@@ -7,6 +7,9 @@ module.exports = {
     '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/'
+  ],
   coverageProvider: 'babel',
   testEnvironment: 'jsdom',
   transform: {
