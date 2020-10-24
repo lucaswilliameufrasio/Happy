4fadb143cc45cba7f23cc2a656deb1df8734e527
Lucas William <lucaswilliameufrasio@gmail.com>
2020-10-24 00:20:10 -0300

    test: ensure RemoteAddOrphanage throws UnexpectedError if HttpClient returns 400

--- web/src/data/usecases/remote-add-orphanage.spec.ts
+++ web/src/data/usecases/remote-add-orphanage.spec.ts
@@ -1,6 +1,8 @@
 import { RemoteAddOrphanage } from './remote-add-orphanage'
+import { HttpStatusCode } from '@/data/protocols/http/http-client'
 import { HttpClientSpy } from '@/data/test/mock-http'
 import { mockAddOrphanageParams } from '@/domain/test/mock-add-orphanage'
+import { UnexpectedError } from '@/domain/errors/unexpected-error'
 import faker from 'faker'
 
 type SutTypes = {
@@ -29,5 +31,17 @@ describe('RemoteAddOrphanage', () => {
     expect(httpClientSpy.url).toBe(url)
     expect(httpClientSpy.method).toBe('post')
     expect(httpClientSpy.body).toBe(addOrphanageParams)
+  })
+
+  test('Should throw UnexpectedError if HttpClient returns 400', async () => {
+    const { sut, httpClientSpy } = makeSut()
+
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.badRequest
+    }
+
+    const promise = sut.add(mockAddOrphanageParams())
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
