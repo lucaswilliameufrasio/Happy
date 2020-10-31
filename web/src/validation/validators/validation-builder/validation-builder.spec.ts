6c1ed1458d625777ed6d1e57c078f75080671216
Lucas William <lucaswilliameufrasio@gmail.com>
2020-10-31 04:34:10 -0300

    test: ensure ValidationBuilder has a required helper

---
+++ web/src/validation/validators/validation-builder/validation-builder.spec.ts
@@ -0,0 +1,12 @@
+import { ValidationBuilder as sut } from './validation-builder'
+import { RequiredFieldValidation } from '@/validation/validators'
+import faker from 'faker'
+
+describe('ValidationBuilder', () => {
+  test('Should return RequiredFieldValidation', () => {
+    const field = faker.database.column()
+    const validations = sut.field(field).required().build()
+
+    expect(validations).toEqual([new RequiredFieldValidation(field)])
+  })
+})
