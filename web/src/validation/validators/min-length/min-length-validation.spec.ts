fb96510cddf6c6c4dc551ea6e293bc983293ccfe
Lucas William <lucaswilliameufrasio@gmail.com>
2020-10-31 04:26:32 -0300

    test: ensure MinLengthValidation returns falsy if value length is greater the specified

--- web/src/validation/validators/min-length/min-length-validation.spec.ts
+++ web/src/validation/validators/min-length/min-length-validation.spec.ts
@@ -2,7 +2,7 @@ import { MinLengthValidation } from './min-length-validation'
 import { InvalidFieldError } from '@/validation/errors'
 import faker from 'faker'
 
-const makeSut = (field: string): MinLengthValidation => new MinLengthValidation(field)
+const makeSut = (field: string): MinLengthValidation => new MinLengthValidation(field, 9)
 
 describe('MinLengthValidation', () => {
   test('Should return error if value length is lesser than the specified', () => {
@@ -12,5 +12,13 @@ describe('MinLengthValidation', () => {
     const error = sut.validate({ [field]: faker.random.alphaNumeric(4) })
 
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should return falsy if value length is greater the specified', () => {
+    const field = faker.database.column()
+    const sut = makeSut(field)
+    const error = sut.validate({ [field]: faker.random.alphaNumeric(10) })
+
+    expect(error).toBeFalsy()
   })
 })
