15c3430764547860e23f851fdebf3decf3f7d7e9
Lucas William <lucaswilliameufrasio@gmail.com>
2020-11-07 02:14:22 -0300

    fix: change url default for storage

--- backend/src/main/config/env.ts
+++ backend/src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   port: process.env.PORT || 7777,
   appUrl: process.env.APP_URL || 'http://localhost:7777',
-  storageUrl: process.env.STORAGE_URL || 'http://localhost:7777/images'
+  storageUrl: process.env.STORAGE_URL || 'http://localhost:7777/image'
 }
