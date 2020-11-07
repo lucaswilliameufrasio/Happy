51c7544003da4c8ddaa5b3fa156c384d22b670eb
Lucas William <lucaswilliameufrasio@gmail.com>
2020-11-07 07:01:50 -0300

    refactor: move sut creation to a factory method

--- web/src/data/usecases/load-orphanages/remote-load-orphanages.spec.ts
+++ web/src/data/usecases/load-orphanages/remote-load-orphanages.spec.ts
@@ -2,11 +2,25 @@ import { RemoteLoadOrphanages } from './remote-load-orphanages'
 import { HttpClientSpy } from '@/data/test/mock-http'
 import faker from 'faker'
 
+type SutTypes = {
+  sut: RemoteLoadOrphanages
+  httpClientSpy: HttpClientSpy
+}
+
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
+  const httpClientSpy = new HttpClientSpy()
+  const sut = new RemoteLoadOrphanages(url, httpClientSpy)
+
+  return {
+    sut,
+    httpClientSpy
+  }
+}
+
 describe('RemoteLoadOrphanages', () => {
   test('Should call HttpClient with correct values', async () => {
     const url = faker.internet.url()
-    const httpClientSpy = new HttpClientSpy()
-    const sut = new RemoteLoadOrphanages(url, httpClientSpy)
+    const { sut, httpClientSpy } = makeSut(url)
     await sut.load()
 
     expect(httpClientSpy.url).toBe(url)
