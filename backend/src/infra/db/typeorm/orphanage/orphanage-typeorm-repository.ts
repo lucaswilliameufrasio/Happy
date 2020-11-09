b08ab0cd50992362ad8290f1dd4f3827b6a482e2
Lucas William <lucaswilliameufrasio@gmail.com>
2020-11-09 12:03:05 -0300

    feat: ensure OrphanageTypeORMRepository loads orphanages on success

--- backend/src/infra/db/typeorm/orphanage/orphanage-typeorm-repository.ts
+++ backend/src/infra/db/typeorm/orphanage/orphanage-typeorm-repository.ts
@@ -1,11 +1,12 @@
+import { OrphanageTypeORMHelper } from './orphanage-typeorm-repository-helper'
 import { TypeORMHelper } from '@/infra/db/typeorm/helpers'
 import { OrphanageEntity } from '@/infra/db/typeorm/entities/orphanage-entity'
 import { AddOrphanageRepository } from '@/data/protocols/db/orphanage/add-orphanage-repository'
+import { LoadOrphanagesRepository } from '@/data/protocols/db/orphanage/load-orphanages-repository'
 import { OrphanageModel } from '@/domain/models/orphanage'
 import { AddOrphanageParams } from '@/domain/usecases/orphanage/add-orphanage'
-import { OrphanageTypeORMHelper } from './orphanage-typeorm-repository-helper'
 
-export class OrphanageTypeORMRepository implements AddOrphanageRepository {
+export class OrphanageTypeORMRepository implements AddOrphanageRepository, LoadOrphanagesRepository {
   constructor (private readonly storageUrl: string) {}
 
   async add (data: AddOrphanageParams): Promise<OrphanageModel> {
@@ -13,5 +14,11 @@ export class OrphanageTypeORMRepository implements AddOrphanageRepository {
     const orphanage = await orphanageRepository.save(data)
 
     return OrphanageTypeORMHelper.assignImageURL(orphanage, this.storageUrl)
+  }
+
+  async load (): Promise<OrphanageModel[]> {
+    const orphanageRepository = await TypeORMHelper.getRepository(OrphanageEntity)
+
+    return orphanageRepository.find()
   }
 }
