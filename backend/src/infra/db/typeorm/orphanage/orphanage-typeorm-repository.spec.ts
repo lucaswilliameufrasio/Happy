ebae946738366386354527512c23a6b2f20b189f
Lucas William <lucaswilliameufrasio@gmail.com>
2020-11-09 11:54:42 -0300

    test: ensure OrphanageTypeORMRepository returns an orphanage on add success

---
+++ backend/src/infra/db/typeorm/orphanage/orphanage-typeorm-repository.spec.ts
@@ -0,0 +1,60 @@
+import { OrphanageTypeORMRepository } from './orphanage-typeorm-repository'
+import { TypeORMHelper } from '@/infra/db/typeorm/helpers'
+import { mockAddOrphanageParams } from '@/domain/test'
+import { createConnection, getConnection } from 'typeorm'
+import faker from 'faker'
+import path from 'path'
+
+export const makeSut = (): OrphanageTypeORMRepository => {
+  return new OrphanageTypeORMRepository(faker.internet.url())
+}
+
+describe('OrphanageTypeORMRepository', () => {
+  beforeAll(async () => {
+    TypeORMHelper.connect = jest.fn().mockImplementationOnce(async () => {
+      await createConnection({
+        type: 'sqlite',
+        database: ':memory:',
+        dropSchema: true,
+        entities: [
+          path.join(__dirname, '..', 'entities/**/!(*.map)')
+        ],
+        synchronize: true,
+        logging: false
+      })
+
+      TypeORMHelper.connection = getConnection()
+    })
+  })
+
+  afterAll(async () => {
+    await TypeORMHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    await TypeORMHelper.connection?.dropDatabase()
+  })
+
+  describe('add()', () => {
+    test('Should return an orphanage on success', async () => {
+      const sut = makeSut()
+
+      const addOrphanageParams = mockAddOrphanageParams()
+
+      const orphanage = await sut.add(addOrphanageParams)
+
+      expect(orphanage).toBeTruthy()
+      expect(orphanage.id).toBeTruthy()
+      expect(orphanage.name).toBe(addOrphanageParams.name)
+      expect(orphanage.latitude).toBe(addOrphanageParams.latitude)
+      expect(orphanage.longitude).toBe(addOrphanageParams.longitude)
+      expect(orphanage.whatsapp).toBe(addOrphanageParams.whatsapp)
+      expect(orphanage.instructions).toBe(addOrphanageParams.instructions)
+      expect(orphanage.about).toBe(addOrphanageParams.about)
+      expect(orphanage.opening_hours).toBe(addOrphanageParams.opening_hours)
+      expect(orphanage.opening_hours).toBe(addOrphanageParams.opening_hours)
+      expect(orphanage.approved).toBe(addOrphanageParams.approved)
+      expect(orphanage.images[0].name).toBe(addOrphanageParams.images[0].name)
+    })
+  })
+})
