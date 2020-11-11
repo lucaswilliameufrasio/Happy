953f645db8df0de14592589af267c46deab51c73
Lucas William <lucaswilliameufrasio@gmail.com>
2020-11-11 08:10:28 -0300

    refactor: change TypeORMHelper connect method to receive an object with ConnectionOptions

--- backend/src/infra/db/typeorm/helpers/typeorm-helper.ts
+++ backend/src/infra/db/typeorm/helpers/typeorm-helper.ts
@@ -1,36 +1,24 @@
-import { createConnection, getConnection, Connection, EntitySchema, Repository } from 'typeorm'
-import path from 'path'
+import { createConnection, getConnection, Connection, EntitySchema, Repository, ConnectionOptions } from 'typeorm'
 
 export const TypeORMHelper = {
   connection: null as Connection,
-  url: null as string,
-  synchronize: null as boolean,
+  connectionOptions: null as ConnectionOptions,
 
-  async connect (
-    url: string,
-    synchronize: boolean = true
-  ): Promise<void> {
-    this.url = url
-    this.synchronize = synchronize
-    await createConnection({
-      type: 'postgres',
-      url: url,
-      entities: [
-        path.join(__dirname, '..', 'entities/**/!(*.map)')
-      ],
-      synchronize: synchronize
-    })
+  async connect (connectionOptions: ConnectionOptions): Promise<void> {
+    this.connectionOptions = connectionOptions
+    await createConnection(connectionOptions)
 
     this.connection = getConnection()
   },
 
   async disconnect () {
     await this.connection.close()
+    this.connection = null
   },
 
   async getRepository (entity: EntitySchema): Promise<Repository<any>> {
     if (!this.connection?.isConnected) {
-      await this.connect(this.url, this.synchronize)
+      await this.connect(this.connectionOptions)
     }
 
     return this.connection.getRepository(entity)
