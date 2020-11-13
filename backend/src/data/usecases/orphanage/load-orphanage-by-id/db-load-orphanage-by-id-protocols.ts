d93f1b6b2bb73f5f752254c529179e45f387aef4
Lucas William <lucaswilliameufrasio@gmail.com>
2020-11-13 00:54:09 -0300

    feat: ensure DbLoadOrphanageById returns an orphanage with correct url

--- backend/src/data/usecases/orphanage/load-orphanage-by-id/db-load-orphanage-by-id-protocols.ts
+++ backend/src/data/usecases/orphanage/load-orphanage-by-id/db-load-orphanage-by-id-protocols.ts
@@ -1,3 +1,4 @@
 export * from '@/data/protocols/db/orphanage/load-orphanage-by-id-repository'
 export * from '@/domain/models/orphanage'
 export * from '@/domain/usecases/orphanage/load-orphanage-by-id'
+export * from '@/data/protocols/storage/storage-service'
--- backend/src/data/usecases/orphanage/load-orphanage-by-id/db-load-orphanage-by-id.ts
+++ backend/src/data/usecases/orphanage/load-orphanage-by-id/db-load-orphanage-by-id.ts
@@ -1,10 +1,18 @@
-import { LoadOrphanageByIdRepository, LoadOrphanageById, OrphanageModel } from './db-load-orphanage-by-id-protocols'
+import { LoadOrphanageByIdRepository, LoadOrphanageById, OrphanageModel, StorageService } from './db-load-orphanage-by-id-protocols'
 
 export class DbLoadOrphanageById implements LoadOrphanageById {
-  constructor (private readonly loadOrphanageByIdRepository: LoadOrphanageByIdRepository) {}
+  constructor (
+    private readonly loadOrphanageByIdRepository: LoadOrphanageByIdRepository,
+    private readonly storageService: StorageService
+  ) {}
 
   async loadById (orphanageId: number): Promise<OrphanageModel> {
     const orphanage = await this.loadOrphanageByIdRepository.loadById(orphanageId)
+    const storageUrl = await this.storageService.getStorageUrl()
+    if (storageUrl) {
+      orphanage.images = orphanage.images.map(image => ({ name: image.name, url: `${storageUrl}/${image.name}` }))
+    }
+
     return orphanage
   }
 }
