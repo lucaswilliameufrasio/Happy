680f79974d5fec06419892725f35d784785724b2
Lucas William <lucaswilliameufrasio@gmail.com>
2020-11-13 00:54:41 -0300

    test: ensure DbLoadOrphanageById returns an orphanage with correct url

--- backend/src/data/usecases/orphanage/load-orphanage-by-id/db-load-orphanage-by-id.spec.ts
+++ backend/src/data/usecases/orphanage/load-orphanage-by-id/db-load-orphanage-by-id.spec.ts
@@ -1,20 +1,23 @@
 import { DbLoadOrphanageById } from './db-load-orphanage-by-id'
-import { LoadOrphanageByIdRepositorySpy } from '@/data/test'
+import { LoadOrphanageByIdRepositorySpy, StorageServiceSpy } from '@/data/test'
 import { throwError } from '@/domain/test'
 import faker from 'faker'
 
 type SutTypes = {
   sut: DbLoadOrphanageById
   loadOrphanageByIdRepositorySpy: LoadOrphanageByIdRepositorySpy
+  storageServiceSpy: StorageServiceSpy
 }
 
 const makeSut = (): SutTypes => {
   const loadOrphanageByIdRepositorySpy = new LoadOrphanageByIdRepositorySpy()
-  const sut = new DbLoadOrphanageById(loadOrphanageByIdRepositorySpy)
+  const storageServiceSpy = new StorageServiceSpy()
+  const sut = new DbLoadOrphanageById(loadOrphanageByIdRepositorySpy, storageServiceSpy)
 
   return {
     sut,
-    loadOrphanageByIdRepositorySpy
+    loadOrphanageByIdRepositorySpy,
+    storageServiceSpy
   }
 }
 
@@ -33,13 +36,6 @@ describe('DbLoadOrphanageById UseCase', () => {
     expect(loadOrphanageByIdRepositorySpy.id).toBe(orphanageId)
   })
 
-  test('Should return an Orphanage on success', async () => {
-    const { sut, loadOrphanageByIdRepositorySpy } = makeSut()
-    const orphanage = await sut.loadById(orphanageId)
-
-    expect(orphanage).toEqual(loadOrphanageByIdRepositorySpy.orphanageModel)
-  })
-
   test('Should throw if LoadOrphanageByIdRepository throws', async () => {
     const { sut, loadOrphanageByIdRepositorySpy } = makeSut()
     jest.spyOn(loadOrphanageByIdRepositorySpy, 'loadById').mockImplementationOnce(throwError)
@@ -47,5 +43,14 @@ describe('DbLoadOrphanageById UseCase', () => {
     const promise = sut.loadById(orphanageId)
 
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should return an Orphanage on success', async () => {
+    const { sut, loadOrphanageByIdRepositorySpy, storageServiceSpy } = makeSut()
+    const orphanage = await sut.loadById(orphanageId)
+    const orphanageImageName = orphanage.images[0].name
+
+    expect(orphanage).toEqual(loadOrphanageByIdRepositorySpy.orphanageModel)
+    expect(orphanage.images[0].url).toEqual(`${storageServiceSpy.url}/${orphanageImageName}`)
   })
 })
