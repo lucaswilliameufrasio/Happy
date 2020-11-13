b4b060a2e648eb9525a5be3b99040d61dd599d0f
Lucas William <lucaswilliameufrasio@gmail.com>
2020-11-13 01:05:36 -0300

    feat: ensure DbLoadOrphanages returns an orphanage with correct url

--- backend/src/data/usecases/orphanage/load-orphanages/db-load-orphanages-protocols.ts
+++ backend/src/data/usecases/orphanage/load-orphanages/db-load-orphanages-protocols.ts
@@ -1,3 +1,4 @@
 export * from '@/data/protocols/db/orphanage/load-orphanages-repository'
 export * from '@/domain/models/orphanage'
 export * from '@/domain/usecases/orphanage/load-orphanages'
+export * from '@/data/protocols/storage/storage-service'
--- backend/src/data/usecases/orphanage/load-orphanages/db-load-orphanages.ts
+++ backend/src/data/usecases/orphanage/load-orphanages/db-load-orphanages.ts
@@ -1,10 +1,21 @@
-import { LoadOrphanagesRepository, LoadOrphanages, OrphanageModel } from './db-load-orphanages-protocols'
+import { LoadOrphanagesRepository, LoadOrphanages, OrphanageModel, StorageService } from './db-load-orphanages-protocols'
 
 export class DbLoadOrphanages implements LoadOrphanages {
-  constructor (private readonly loadOrphanagesRepository: LoadOrphanagesRepository) {}
+  constructor (
+    private readonly loadOrphanagesRepository: LoadOrphanagesRepository,
+    private readonly storageService: StorageService
+  ) {}
 
   async load (): Promise<OrphanageModel[]> {
-    const orphanage = await this.loadOrphanagesRepository.load()
-    return orphanage
+    let orphanages = await this.loadOrphanagesRepository.load()
+    const storageUrl = await this.storageService.getStorageUrl()
+    if (storageUrl) {
+      orphanages = orphanages.map(orphanage => {
+        orphanage.images = orphanage.images.map(image => ({ name: image.name, url: `${storageUrl}/${image.name}` }))
+
+        return orphanage
+      })
+    }
+    return orphanages
   }
 }
