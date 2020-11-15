686934298f0e8419e1daa690dcd62ca05ccd00bd
Lucas William <lucaswilliameufrasio@gmail.com>
2020-11-15 15:09:21 -0300

    feat: ensure Orphanage view calls loadOrphanagesById

--- web/src/presentation/pages/orphanage/orphanage.tsx
+++ web/src/presentation/pages/orphanage/orphanage.tsx
@@ -1,15 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaWhatsapp } from 'react-icons/fa'
 import { FiClock, FiInfo } from 'react-icons/fi'
 import { Map, Marker, TileLayer } from 'react-leaflet'
+import { useHistory } from 'react-router-dom'
 
 import leafletMapIcon from '@/presentation/helpers/leaflet-map-icon-config'
 import { Sidebar } from '@/presentation/components'
+import { LoadOrphanageById } from '@/domain/usecases/load-orphanage-by-id'
 
 import './orphanage.css'
 
-export default function Orphanage () {
-  const openOnWeekends = typeof '' === 'string'
+type Props = {
+  loadOrphanageById: LoadOrphanageById
+}
+
+export default function Orphanage ({ loadOrphanageById }: Props) {
+  const history = useHistory()
+  const [orphanage, updateOrphanageData] = useState<LoadOrphanageById.Model>({
+    id: null,
+    name: null,
+    about: null,
+    approved: null,
+    images: [{ name: null, url: null }],
+    instructions: null,
+    latitude: -3.2081546,
+    longitude: -52.2262043,
+    open_on_weekend: null,
+    opening_hours: null,
+    whatsapp: null
+  })
+  const [imageActiveIndex, setImageActiveIndex] = useState(0)
+
+  useEffect(() => {
+    loadOrphanageById.loadById()
+      .then(orphanageData => updateOrphanageData(orphanageData))
+      .catch(error => {
+        console.log(error)
+        history.push('/app')
+      })
+  }, [])
+
+  function handleWhatsappContact () {
+    window.location.href = `https://api.whatsapp.com/send?phone=${orphanage.whatsapp}&text=Olá,%20${orphanage.name}%20gostaria%20de%20agendar%20um%20dia%20para%20visitar%20o%20orfanato.`
+  }
 
   return (
     <div id="page-orphanage">
@@ -17,36 +50,28 @@ export default function Orphanage () {
 
       <main>
         <div className="orphanage-details">
-          <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
+          <img src={orphanage.images[imageActiveIndex].url} alt={orphanage.name} />
 
           <div className="images">
-            <button className="active" type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-            </button>
-            <button type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-            </button>
-            <button type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-            </button>
-            <button type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-            </button>
-            <button type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-            </button>
-            <button type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-            </button>
+            {orphanage.images.map((image, index) => (
+              <button
+                key={image.name}
+                className={imageActiveIndex === index ? 'active' : ''}
+                type="button"
+                onClick={() => setImageActiveIndex(index)}
+              >
+                <img src={image.url} alt={orphanage.name} />
+              </button>
+            ))}
           </div>
 
           <div className="orphanage-details-content">
-            <h1>Lar das meninas</h1>
-            <p>Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.</p>
+            <h1>{orphanage.name}</h1>
+            <p>{orphanage.about}</p>
 
             <div className="map-container">
               <Map
-                center={[-3.2081546, -52.2261614]}
+                center={[orphanage.latitude, orphanage.longitude]}
                 zoom={16}
                 style={{ width: '100%', height: 280 }}
                 dragging={false}
@@ -58,26 +83,26 @@ export default function Orphanage () {
                 <TileLayer
                   url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker interactive={false} icon={leafletMapIcon} position={[-3.2081546, -52.2261614]} />
+                <Marker interactive={false} icon={leafletMapIcon} position={[orphanage.latitude, orphanage.longitude]} />
               </Map>
 
               <footer>
-                <a href="">Ver rotas no Google Maps</a>
+                <a rel="noopener noreferrer" href={`http://maps.google.com/?q=${orphanage.latitude},${orphanage.longitude}`}>Ver rotas no Google Maps</a>
               </footer>
             </div>
 
             <hr />
 
             <h2>Instruções para visita</h2>
-            <p>Venha como se sentir mais à vontade e traga muito amor para dar.</p>
+            <p>{orphanage.instructions}</p>
 
             <div className="open-details">
               <div className="hour">
                 <FiClock size={32} color="#15B6D6" />
                 Segunda à Sexta <br />
-                8h às 18h
+                {orphanage.opening_hours}
               </div>
-              {openOnWeekends ? (
+              {orphanage.open_on_weekend ? (
                 <div className="open-on-weekend">
                   <FiInfo size={32} color="#39CC83" />
                 Atendemos <br />
@@ -92,10 +117,10 @@ export default function Orphanage () {
               )}
             </div>
 
-            <button type="button" className="contact-button">
+            <a rel="noopener noreferrer" target="_blank" type="button" className="contact-button" onClick={handleWhatsappContact}>
               <FaWhatsapp size={20} color="#FFF" />
               Entrar em contato
-            </button>
+            </a>
           </div>
         </div>
       </main>
