bf367f6ac8d8e6612c9832a7baa25a40b28c33b1
Lucas William <lucaswilliameufrasio@gmail.com>
2020-11-16 21:51:26 -0300

    feat: add OrphanagesMap screen

--- mobile/src/main/index.tsx
+++ mobile/src/main/index.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { useFonts } from 'expo-font'
 import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito'
 import { ActivityIndicator } from 'react-native'
+import OrphanagesMap from '../presentation/screens/orphanages-map/orphanages-map'
 
 function Main (): JSX.Element {
   const [fontsLoaded] = useFonts({
@@ -16,7 +17,7 @@ function Main (): JSX.Element {
   }
 
   return (
-    <View />
+    <OrphanagesMap />
   )
 }
 
---
+++ mobile/src/presentation/screens/orphanages-map/orphanages-map.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps'
+import { Feather } from '@expo/vector-icons'
+import styles from './orphanages-map-styles'
+
+import mapMarker from '../../../../assets/map-marker.png'
+
+const OrphanagesMap = (): JSX.Element => {
+  return (
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: -3.2081546,
+          longitude: -52.2262043,
+          latitudeDelta: 0.009,
+          longitudeDelta: 0.009
+        }}
+      >
+        <Marker
+          icon={mapMarker}
+          calloutAnchor={{
+            x: 2.7,
+            y: 0.8
+          }}
+          coordinate={{
+            latitude: -3.2081546,
+            longitude: -52.2262043
+          }}
+        >
+          <Callout tooltip onPress={() => {}}>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Alou</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>1 orfanato encontrado</Text>
+        <TouchableOpacity style={styles.createOrphanageButton} onPress={() => {}}>
+          <Feather name="plus" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+export default OrphanagesMap
