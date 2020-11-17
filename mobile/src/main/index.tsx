ce9d8704a449b84efe66478b052e310c245399f3
Lucas William <lucaswilliameufrasio@gmail.com>
2020-11-16 21:51:09 -0300

    feat: load fonts on app start

--- mobile/src/main/index.tsx
+++ mobile/src/main/index.tsx
@@ -1,8 +1,20 @@
 import { registerRootComponent } from 'expo'
 import React from 'react'
-import { View } from 'react-native'
+import { useFonts } from 'expo-font'
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito'
+import { ActivityIndicator } from 'react-native'
 
 function Main (): JSX.Element {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold
+  })
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />
+  }
+
   return (
     <View />
   )
