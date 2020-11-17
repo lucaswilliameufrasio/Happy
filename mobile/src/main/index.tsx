c4f64717ce8794687fcfceba3076f707563447d3
Lucas William <lucaswilliameufrasio@gmail.com>
2020-11-16 21:19:37 -0300

    refactor: move app entrypoint to main layer

---
+++ mobile/src/main/index.tsx
@@ -0,0 +1,11 @@
+import { registerRootComponent } from 'expo'
+import React from 'react'
+import { View } from 'react-native'
+
+function Main (): JSX.Element {
+  return (
+    <View />
+  )
+}
+
+registerRootComponent(Main)
