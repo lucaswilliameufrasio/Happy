b82c4e3c24a4190ccf52838149b50fc43261e0fc
Lucas William <lucaswilliameufrasio@gmail.com>
2020-11-18 00:40:54 -0300

    style: sort stylesheet objects in ascending order

--- mobile/src/presentation/components/header/header-styles.ts
+++ mobile/src/presentation/components/header/header-styles.ts
@@ -2,25 +2,25 @@ import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
   container: {
-    padding: 24,
+    alignItems: 'center',
     backgroundColor: '#F9FAFC',
     borderBottomWidth: 1,
     borderColor: '#DDE3F0',
-    paddingTop: 44,
-
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
 
-  title: {
-    fontFamily: 'Nunito_600SemiBold',
-    color: '#8FA7B3',
-    fontSize: 16
+    justifyContent: 'space-between',
+    padding: 24,
+    paddingTop: 44
   },
 
   hiddenContainer: {
     width: 21
+  },
+
+  title: {
+    color: '#8FA7B3',
+    fontFamily: 'Nunito_600SemiBold',
+    fontSize: 16
   }
 })
 
